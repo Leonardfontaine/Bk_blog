e962ea2f56c7ae8e846c21067e52678201d7f258
Leonardfontaine <49778032+Leonardfontaine@users.noreply.github.com>
2019-05-30 09:52:27 +0200

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '18.136.224.43',
 	'username' => 'root',
 	'password' => 'pwdpwd',
 	'database' => 'bkblog',
